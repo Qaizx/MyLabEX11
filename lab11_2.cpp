a0e9033256ebdf1bdc2faf11d34b419077fcf568
Qaizx <kaothom@gmail.com>
2021-01-15 15:18:28 +0700

    finish

--- lab11_2.cpp
+++ lab11_2.cpp
@@ -1,9 +1,24 @@
 #include<iostream>
 using namespace std;
 
+int gcd(int x,int y);
+
 int main(){
     int x, y;
     cin >> x >> y;
     cout << gcd(x,y);
     return 0; 
+}
+
+int gcd(int x, int y){
+
+    int a = 0;
+    
+    if(x % y !=0)
+        a = gcd(y, x%y);
+    else
+        a = y;
+
+    return a;
+    
 }
