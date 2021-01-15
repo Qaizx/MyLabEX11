b034da3efd1b3b25d7385056fa9b495b5d88f8a4
Qaizx <kaothom@gmail.com>
2021-01-15 15:46:39 +0700

    finish

--- lab11_4.cpp
+++ lab11_4.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 
 using namespace std;
+template <typename currentType>
+void mySwap(currentType &,currentType &); 
 
 int main(){
 	int x, y;
@@ -30,3 +32,10 @@ int main(){
 	return 0;
 }
 
+template <typename currentType>
+void mySwap(currentType &x, currentType &y){
+	currentType a = x;
+	currentType b = y;
+	x = b;
+	y = a;
+}
